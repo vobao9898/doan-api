69c2bace5fb5d82f7c6a1d2bff2093fc2b7ec0ad
vobao989898 <95485049+vobao9898@users.noreply.github.com>
2023-04-10 10:51:01 +0700

    upda

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,21 @@
+
 const { createPool } = require('mysql');
 
+// const pool = createPool({
+//     host: 'us-cdbr-east-05.cleardb.net',
+//     user: 'bc64f62de970d8',
+//     password: 'e92eda4e',
+//     database: 'heroku_5ec5c13a06479b3',
+//     connectionLimit: 10,
+// });
+
+
 const pool = createPool({
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'bc64f62de970d8',
-    password: 'e92eda4e',
-    database: 'heroku_5ec5c13a06479b3',
-    connectionLimit: 10,
+    connectionLimit: 11,
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bf0844fbd86225',
+    password: '71b99177238a174',
+    database: 'heroku_b938c71e4878655',
 });
 
 module.exports = pool;
